f3be8c413e6684643273900469561240804f6d6e
Partrucci <Partrucci@gmail.com>
2020-03-05 01:20:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 app.get('/', function(req, res, next){
-    res.send('Hola Amegos!');
+    res.end('Hola Amegos!');
 });
 
 app.listen(port, hostname, () =>{
