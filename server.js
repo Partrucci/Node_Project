af18df2ee7e216f87d51a7d317b32facfa7221fa
Partrucci <61731030+Partrucci@users.noreply.github.com>
2020-03-06 17:52:17 +0700

    add index.html and server.js

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get('/', function(req, res, next){
-    res.end('Hola Amegos!');
+//app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    console.log('this is app.get');
+    res.sendFile(__dirname+'/index.html');
 });
 
-app.listen(port, hostname, () =>{
+server.listen(port, () =>{
+    console.log('this is server.listen');
     console.log(`the server is running on ${hostname}:${port}`);
+});
+
+io.on('connection', function(socket){
+    console.log('a CHAD connected');
+    socket.on('disconnect', function(){
+        console.log('a CHAD has gone... left only virgin!');
+    });
 });
