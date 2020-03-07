9ead91f16465a4414413d345f125025e7fa2f7fb
Partrucci <Partrucci@gmail.com>
2020-03-08 02:21:53 +0700

    update user

--- server.js
+++ server.js
@@ -7,32 +7,37 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 app.use(express.static('public'));
-
+/*
 app.get('/',function(req, res){
     console.log('VIRGIN DETECTED')
     res.write('HELLO VIRGIN!');    
     res.end();
 });
-
-app.get('/CHADroom', function(req, res){
+*/
+app.get('/', function(req, res){
     console.log('Enter the CHADroom');
     res.sendFile(__dirname + '/chatroom.html');
 });
 
-server.listen(port, () =>{
-    console.log('this is server.listen');
-    console.log(`the server is running on ${hostname}:${port}`);
-});
-
 io.on('connection', function(socket){
     console.log('a CHAD connected');
-    socket.on('disconnect', function(){
+
+    socket.on('username', function(username){
+        socket.username = username;
+        io.emit('online','<center><i>'+socket.username+' join the chat...</i></center>');
+    });
+
+    socket.on('disconnect', function(username){
         console.log('a CHAD has gone... left only virgin!');
+        io.emit('online,','<center><i>'+socket.username+'left the chat...</i></center>');
     });
 
     socket.on('chatting',function(msg){
         console.log('a CHAD says :',msg);
-
-        io.emit('chatting',msg);
+        io.emit('chatting',socket.username+' : '+msg);
     });
+});
+
+server.listen(port, () =>{
+    console.log(`the server is running on ${hostname}:${port}`);
 });
