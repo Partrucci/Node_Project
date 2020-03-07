320074c176bc3fb71706fa3eea53027380ff7bb4
Partrucci <61731030+Partrucci@users.noreply.github.com>
2020-03-07 17:25:07 +0700

    add more event

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
-app.get('/', function(req, res){
-    console.log('this is app.get');
-    res.sendFile(path.join(__dirname,'/index.html'));
+app.get('/',function(req, res){
+    console.log('VIRGIN DETECTED')
+    res.write('HELLO VIRGIN!');    
+    res.end();
+});
+
+app.get('/CHADroom', function(req, res){
+    console.log('Enter the CHADroom');
+    res.sendFile(__dirname + '/chatroom.html');
 });
 
 server.listen(port, () =>{
@@ -23,5 +28,9 @@ io.on('connection', function(socket){
     console.log('a CHAD connected');
     socket.on('disconnect', function(){
         console.log('a CHAD has gone... left only virgin!');
+    });
+
+    socket.on('asdasdasd',function(msg){
+        console.log('a CHAD says :',msg);
     });
 });
