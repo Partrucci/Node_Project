dbff30e2d2d2cbf51b649e8bbfe81f5828fa44ba
Partrucci <Partrucci@gmail.com>
2020-03-08 22:35:49 +0700

    latestchat update

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const session = require('express-session');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(session({
+    secret: 'chatsession',
+    saveUninitialized: true,
+    resave: true
+}));
 /*
 app.get('/',function(req, res){
     console.log('VIRGIN DETECTED')
@@ -19,22 +25,46 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/chatroom.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a CHAD connected');
+app.get('/chatstore', function(req, res){
+    res.send(global.username+' : '+global.latestmessage);
+});
 
+io.on('connection', function(socket){
+    console.log('a user connected');
+    /*
+    client.push({id : socket.client.id});
+    console.log(client);
+
+    var getClientID = client.find(e => (e.id === socket.client.id));
+    if(getClientID){
+//        socket.on('latestmessage',history);
+    }
+*/
     socket.on('username', function(username){
         socket.username = username;
         io.emit('online','<center><i>'+socket.username+' join the chat...</i></center>');
+        console.log(`User ${socket.username} connected`);
     });
 
     socket.on('disconnect', function(username){
         io.emit('online','<center><i>'+socket.username+' left the chat...</i></center>');
+        console.log(`User ${socket.username} disconnected`);
     });
 
-    socket.on('chatting',function(msg){
-        console.log('a CHAD says :',msg);
+    socket.on('chatting', function(msg){
+        global.latestmessage = msg;
+        global.username = socket.username;
+//        history.push(socket.username+' : '+msg);
+//        console.log('latest chat session : ',history);
         io.emit('chatting',socket.username+' : '+msg);
     });
+/*
+    socket.on('chathistory',function(username){
+        history.push(username);
+        console.log(history);
+        io.emit(username);
+    });
+  */      
 });
 
 server.listen(port, () =>{
